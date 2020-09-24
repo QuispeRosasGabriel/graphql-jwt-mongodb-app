152039d71c7ea4b9f76a4ed40eba10fb70345871
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-24 00:44:49 -0500

    seteando schemas y resolvers

---
+++ src/resolvers/mutation.ts
@@ -0,0 +1,17 @@
+import { IResolvers } from "graphql-tools";
+
+const mutation: IResolvers = {
+  Mutation: {
+    // hola(): string {
+    //   return "Hola Mundoddddd!";
+    // },
+    // holaConNombre(__: void, { nombre }): string {
+    //   return `Hola Mundo ${nombre}`;
+    // },
+    // holaAlCursoGraphQL(): string {
+    //   return "Hola Mundo en el curso de GraphQL";
+    // },
+  },
+};
+
+export default mutation;
