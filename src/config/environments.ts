fdc482820bbb401193b2e86d37867bd99ccf7406
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-23 23:19:38 -0500

    configurando env y datetime

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,4 +1,9 @@
 import dotenv from "dotenv";
 const environments = dotenv.config({ path: "./src/.env" });
+if (process.env.NODE_ENV !== "production") {
+  if (environments.error) {
+    throw environments.error;
+  }
+}
 
 export default environments;
