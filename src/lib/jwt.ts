796a52a4cdf8f1674bac461d571a724dad143e80
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-24 23:48:51 -0500

    preparando jwt

---
+++ src/lib/jwt.ts
@@ -0,0 +1,20 @@
+import { SECRET_KEY } from "../config/constants";
+import jwt from "jsonwebtoken";
+
+export class JWT {
+  private secretKey = SECRET_KEY as string;
+
+  sign(data: any): string {
+    return jwt.sign({ user: data.user }, this.secretKey, {
+      expiresIn: 24 * 60 * 60,
+    });
+  }
+
+  verify(token: string): string {
+    try {
+      return jwt.verify(token, this.secretKey) as string;
+    } catch (error) {
+      return "La autenticación del token es invalida";
+    }
+  }
+}
