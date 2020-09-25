3a183c711977de2d54411a31c4d714b78994b0e2
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-24 23:13:13 -0500

    login graphql

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -5,6 +5,10 @@ const query: IResolvers = {
     async users(_: void, __: any, { db }): Promise<any> {
       return await db.collection("users").find().toArray();
     },
+
+    async login(_: void, { email, password }, { db }): Promise<any> {
+      return await db.collection("users").findOne({ email, password });
+    },
   },
 };
 
