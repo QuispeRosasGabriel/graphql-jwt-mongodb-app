876e9a31840067d8a0a1b714b166fc04ddc03911
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-24 23:38:53 -0500

    controlando escenarios login

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -7,7 +7,30 @@ const query: IResolvers = {
     },
 
     async login(_: void, { email, password }, { db }): Promise<any> {
-      return await db.collection("users").findOne({ email, password });
+      return await db
+        .collection("users")
+        .findOne({ email, password })
+        .then((res: any) => {
+          if (res === null) {
+            return {
+              status: false,
+              message: "Login Incorrecto, comprueba la información",
+              user: null,
+            };
+          }
+          return {
+            status: true,
+            message: "Login Correcto",
+            user: res,
+          };
+        })
+        .catch((err: any) => {
+          return {
+            status: false,
+            message: "Error inesperado",
+            user: "null",
+          };
+        });
     },
   },
 };
